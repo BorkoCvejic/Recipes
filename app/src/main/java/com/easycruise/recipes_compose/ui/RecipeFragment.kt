1c58f62d6d89129ed61fe4830307864c4fec5c32
Borko Cvejic <borko.cvejic@zesium.com>
2021-08-03 20:57:30 +0200

    Initial push, fragments setup

---
+++ app/src/main/java/com/easycruise/recipes_compose/ui/RecipeFragment.kt
@@ -0,0 +1,87 @@
+package com.easycruise.recipes_compose.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.easycruise.recipes_compose.R
+
+class RecipeFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            color = Color(ContextCompat.getColor(LocalContext.current, R.color.lightGray))
+                        )
+                        .verticalScroll(
+                            state = rememberScrollState()
+                        )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_ice_cream),
+                        contentDescription = null,
+                        Modifier.height(300.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = "Happy meal",
+//                color = Color.White,
+                                fontSize = 26.sp
+                            )
+                            Text(text = "$5.99",
+                                style = TextStyle(
+                                    color = Color(ContextCompat.getColor(LocalContext.current, R.color.lightGreen)),
+                                    fontSize = 17.sp
+                                ),
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                        }
+                        Spacer(modifier = Modifier.padding(top = 10.dp))
+                        Text(
+                            text = "800 Calories",
+                            fontSize = 17.sp
+                        )
+                        Spacer(modifier = Modifier.padding(top = 10.dp))
+                        Button(
+                            onClick = {},
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                        ) {
+                            Text(text = LocalContext.current.getString(R.string.label_order_now))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
