30eb3feb3d82e2c351d47b656804880c46dfebe3
Borko Cvejic <borkocvejic@gmail.com>
2021-08-06 15:48:17 +0200

    Added list of recipes (LazyColumn)

--- app/src/main/java/com/easycruise/recipes_compose/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/easycruise/recipes_compose/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,31 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.easycruise.recipes_compose.R
+import com.easycruise.recipes_compose.presentation.components.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RecipeListFragment: Fragment() {
 
-    val viewModel: RecipeListViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        println("FRAGMENT:  ${viewModel.getRepo()} ")
-        println("FRAGMENT:  ${viewModel.getToken()} ")
-    }
+    private val viewModel: RecipeListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,17 +26,18 @@ class RecipeListFragment: Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "Recipe List",
-                        fontSize = 21.sp
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(
-                        onClick = {
-                            findNavController().navigate(R.id.showRecipe) }
-                    ) {
-                        Text(text = "TO RECIPE FRAGMENT")
+
+                val recipes = viewModel.recipes.value
+
+                LazyColumn {
+                    itemsIndexed(
+                        items = recipes
+                    ){ index, recipe ->
+                        RecipeCard(
+                            recipe = recipe,
+                            onClick = {
+                                findNavController().navigate(R.id.showRecipe)
+                        })
                     }
                 }
             }
--- app/src/main/java/com/easycruise/recipes_compose/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/easycruise/recipes_compose/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -1,8 +1,13 @@
 package com.easycruise.recipes_compose.presentation.ui.recipe_list
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.easycruise.recipes_compose.domain.model.Recipe
 import com.easycruise.recipes_compose.repository.RecipeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -14,12 +19,29 @@ constructor(
     private @Named("auth_token") val token: String
 ): ViewModel() {
 
+    //standard way of getting data from response
+//    private val _recipes: MutableLiveData<List<Recipe>> = MutableLiveData() //can be changed
+//
+//    val recipes: LiveData<List<Recipe>> get() = _recipes //read only, observe this in fragment
+
+
+    //better way for compose
+    val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf()) //can be changed
+
+
     init {
-        println("VIEWMODEL:  $repository ")
-        println("VIEWMODEL:  $token ")
+        newSearch()
     }
 
-    fun getRepo() = repository
+    private fun newSearch() {
+        viewModelScope.launch {
+            val result = repository.search(
+                token = token,
+                page = 1,
+                query = "chicken"
+            )
+            recipes.value = result
 
-    fun getToken() = token
+        }
+    }
 }
